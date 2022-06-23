a0bf912fc5105567d9baf7ec8956036155015402
PaulSensor <sensor445@gmail.com>
2022-06-23 23:27:41 +0300

    feat: add solution task Human Readable Number in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
+    const arrNum = number.toString().split('')
+    let sliceNum = +arrNum.slice(1).join('')
+    let result
+    
+    const n1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    const n2 = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const n3 = ['ten', 'twenty', 'thirty', 'forty', 'fifty','sixty', 'seventy', 'eighty', 'ninety'] 
+
+    if (arrNum.length === 1) {
+        result = `${n1[number]}`
+    } else if (arrNum.length === 2 && number < 20 && sliceNum !== 0) {
+        result = `${n2[sliceNum - 1]}`
+    } else if (arrNum.length === 2 && number > 20 && sliceNum !== 0) {
+        result = `${n3[Math.trunc(number / 10) - 1]} ${n1[sliceNum]}`
+    } else if (arrNum.length === 2 && number % 10 === 0) {
+        result = `${n3[(number / 10) - 1]}`
+    } else if (arrNum.length == 3 && sliceNum < 10 && sliceNum !== 0) {
+        result = `${n1[Math.trunc(number / 100)]} hundred ${n1[number % 100]}`
+    } else if (arrNum.length === 3 && sliceNum < 20 && sliceNum % 10 !== 0) {
+        result = `${n1[Math.trunc(number / 100)]} hundred ${n2[(number % 100) - 11]}`
+    } else if (arrNum.length === 3 && sliceNum > 20 && sliceNum % 10 !== 0) {
+        result = `${n1[Math.trunc(number / 100)]} hundred ${n3[Math.trunc((number % 100) / 10) - 1]} ${n1[(number % 100) % 10]}`
+    } else if (arrNum.length === 3 && number % 100 === 0) {
+        result = `${n1[number / 100]} hundred`
+    } else if (arrNum.length === 3 && sliceNum % 10 === 0) {
+        result = `${n1[Math.trunc(number / 100)]} hundred ${n3[(sliceNum / 10) - 1]}`
+    }
+
+    return result.trim()
 }
